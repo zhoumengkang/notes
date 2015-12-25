6fa5308dff6acbf1168a6c4510e313d48b74cae5
zhoumengkang <i@zhoumengkang.com>
2015-12-25 22:35:40 +0800

    把单例删了，支持多线程并发调用

--- java/yartest/src/main/java/yar/concurrent/client/YarConcurrentErrorCallback.java
+++
@@ -1,8 +0,0 @@
-package yar.concurrent.client;
-
-/**
- * Created by zhoumengkang on 23/12/15.
- */
-public abstract class YarConcurrentErrorCallback {
-    abstract void error();
-}
---
+++ java/yartest/src/test/java/net/mengkang/yar/client/YarClientTest.java
@@ -0,0 +1,29 @@
+package net.mengkang.yar.client;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by zhoumengkang on 25/12/15.
+ */
+public class YarClientTest extends TestCase {
+    /**
+     * 定义 rpc 接口
+     */
+    public interface RewardScoreService{
+        String support(int uid,int fid);
+        String post(int uid,int fid);
+    }
+
+    /**
+     * rpc api 地址
+     */
+    static String RewardScoreServiceUri = "http://mengkang.net/demo/yar-server/RewardScoreService.php";
+
+    public void testUserService(){
+        YarClient yarClient  = new YarClient(RewardScoreServiceUri);
+        RewardScoreService rewardScoreService = (RewardScoreService) yarClient.useService(RewardScoreService.class);
+        System.out.println(rewardScoreService.support(1, 2));
+        System.out.println(rewardScoreService.post(1, 20));
+    }
+
+}
